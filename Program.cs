cea6c27a35abd3e2f8aec2e469fd84258e819d39
learningpath2025 <learningpath2025@outlook.com>
2025-09-08 09:56:33 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Assignment3._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetInputAndOutput();
+            //TestChangeConsecutiveOnesToZeros();
+        }
+
+        static void GetInputAndOutput()
+        {
+            Console.Write("Enter the size of the array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("Enter the elements of the array:");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int[] result = ChangeConsecutiveOnesToZeros(arr);
+            Console.WriteLine($"Output: [{string.Join(", ", result)}]");
+        }
+
+        static void TestChangeConsecutiveOnesToZeros()
+        {
+            int[] input = { 0, 2, 1, 1, 9, 1, 1 };
+            int[] expectedOutput = { 0, 2, 0, 0, 9, 1, 1 };
+            int[] actualOutput = ChangeConsecutiveOnesToZeros(input);
+            if (actualOutput.SequenceEqual(expectedOutput))
+            {
+                Console.WriteLine("Test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Test failed.");
+            }
+        }
+
+        static int[] ChangeConsecutiveOnesToZeros(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == 1 && arr[i + 1] == 1)
+                {
+                    arr[i] = 0;
+                    arr[i + 1] = 0;
+                    break; // Only change the first occurrence
+                }
+            }
+            return arr;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Assignment3.1.4
+
+- Write a function which takes an array as input and finds the first occurrence of 2 consecutive 1s and changes their value to 0.
+
+- For e.g: Input : [0,2,1,1,9,1,1]
+
+- Output: [0,2,0,0,9,1,1]
